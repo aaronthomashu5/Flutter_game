6a9abbacc7e1a314aeb1cd4dae531ee642442346
Aaron <124760049+aaronthomashu5@users.noreply.github.com>
2024-02-02 22:42:48 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flame/game.dart';
+import 'package:flutter/material.dart';
+import '/brick_breaker.dart';
+
+import '/widgets/game_app.dart';
+
+void main() {
+  runApp(const GameApp());
+}
